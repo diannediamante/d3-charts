a7a1fda09ae55a98dcbea184797fc911e3f7ca2c
diannediamante <mddiamante@up.edu.ph>
2021-07-19 01:00:13 +0800

    Add Barchart component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect, useState, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
-import { select, axisBottom, axisRight, scaleLinear, scaleBand } from 'd3';
+// import { select, axisBottom, axisRight, scaleLinear, scaleBand } from 'd3';
+import Barchart from './Barchart';
 
 //line = for line elements
 
@@ -8,73 +9,73 @@ import { select, axisBottom, axisRight, scaleLinear, scaleBand } from 'd3';
 function App() {
 
   const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75])  //dynamic data
-  const svgRef = useRef();
+  // const svgRef = useRef();
 
-  //called once when DOM elements is rendered
-  useEffect(() => {
-    const svg = select(svgRef.current);
+  // //called once when DOM elements is rendered
+  // useEffect(() => {
+  //   const svg = select(svgRef.current);
 
-    const xScale = scaleBand()
-      .domain(data.map((value, index) => index))  //discrete, 0-0, 1-60..., should be explicit
-      .range([0, 300])
-      .padding(0.5)
+  //   const xScale = scaleBand()
+  //     .domain(data.map((value, index) => index))  //discrete, 0-0, 1-60..., should be explicit
+  //     .range([0, 300])
+  //     .padding(0.5)
     
-    const yScale = scaleLinear()
-      .domain([0, 150])
-      .range([150, 0])
+  //   const yScale = scaleLinear()
+  //     .domain([0, 150])
+  //     .range([150, 0])
 
-    const colorScale = scaleLinear()
-      .domain([75, 100, 150])
-      .range(["green", "orange", "red"])
-      .clamp(true)
+  //   const colorScale = scaleLinear()
+  //     .domain([75, 100, 150])
+  //     .range(["green", "orange", "red"])
+  //     .clamp(true)
     
-    //create x-axis
-    const xAxis = axisBottom(xScale)
-                      .ticks(data.length) // .tickFormat(index => index + 1);
+  //   //create x-axis
+  //   const xAxis = axisBottom(xScale)
+  //                     .ticks(data.length) // .tickFormat(index => index + 1);
 
-    svg
-      .select(".x-axis")
-      .style("transform", "translateY(150px")
-      .call(xAxis);
+  //   svg
+  //     .select(".x-axis")
+  //     .style("transform", "translateY(150px")
+  //     .call(xAxis);
     
-    //create y-axis
-    const yAxis = axisRight(yScale)
+  //   //create y-axis
+  //   const yAxis = axisRight(yScale)
 
-    svg
-      .select(".y-axis")
-      .style("transform", "translateX(300px")
-      .call(yAxis);
+  //   svg
+  //     .select(".y-axis")
+  //     .style("transform", "translateX(300px")
+  //     .call(yAxis);
 
 
-    // draw the bars
-    // console.log(data)
-    svg.selectAll(".bar")
-      .data(data)
-      .join("rect")
-      .attr("class", "bar")
-      .style("transform", "scale(1, -1")  //flip upside down y axis
-      .attr("x", (value, index) => xScale(index)) //index refers to data "data"
-      .attr("y", -150)                
-      .attr("width", xScale.bandwidth())      //bandwidth = width of each bar
-      .on("mouseenter", (event, value) => {
-        console.log(value)
-        const index = svg.selectAll(".bar").nodes().indexOf(event.target);
-        svg.selectAll(".tooltip")
-          .data([value])
-          .join(enter => enter.append("text").attr("y", yScale(value) - 4))
-          .attr("class", "tooltip")
-          .text(value)
-          .attr("x", xScale(index) + xScale.bandwidth() / 2) 
+  //   // draw the bars
+  //   // console.log(data)
+  //   svg.selectAll(".bar")
+  //     .data(data)
+  //     .join("rect")
+  //     .attr("class", "bar")
+  //     .style("transform", "scale(1, -1")  //flip upside down y axis
+  //     .attr("x", (value, index) => xScale(index)) //index refers to data "data"
+  //     .attr("y", -150)                
+  //     .attr("width", xScale.bandwidth())      //bandwidth = width of each bar
+  //     .on("mouseenter", (event, value) => {
+  //       console.log(value)
+  //       const index = svg.selectAll(".bar").nodes().indexOf(event.target);
+  //       svg.selectAll(".tooltip")
+  //         .data([value])
+  //         .join(enter => enter.append("text").attr("y", yScale(value) - 4))
+  //         .attr("class", "tooltip")
+  //         .text(value)
+  //         .attr("x", xScale(index) + xScale.bandwidth() / 2) 
           
-          .attr("text-anchor", "middle")
-          .transition()
-          .attr("y", yScale(value) - 8)
-          .attr("opacity", 1)
-      })
-      .on("mouseleave", () => svg.select(".tooltip").remove())
-      .transition()
-      .attr("fill", colorScale)
-      .attr("height", value => 150 - yScale(value))                     //height = height of svg - actual height
+  //         .attr("text-anchor", "middle")
+  //         .transition()
+  //         .attr("y", yScale(value) - 8)
+  //         .attr("opacity", 1)
+  //     })
+  //     .on("mouseleave", () => svg.select(".tooltip").remove())
+  //     .transition()
+  //     .attr("fill", colorScale)
+  //     .attr("height", value => 150 - yScale(value))                     //height = height of svg - actual height
       
 
 
@@ -82,15 +83,16 @@ function App() {
 
 
 
-  }, [data]);
+  // }, [data]);
 
 
   return (
     <Fragment>
-      <svg ref={svgRef}>
+      {/* <svg ref={svgRef}>
         <g className="x-axis"></g>
         <g className="y-axis"></g>
-      </svg>
+      </svg> */}
+      <Barchart data={data} />
       <br/>
       <button onClick={()=> setData(data.map(value => value + 5))}>Update Data</button>
       <button onClick={()=> setData(data.filter(value => value < 35))}>Filter Data</button>
