8ad47e8b2d57608560134b1931997c31e7d53836
diannediamante <mddiamante@up.edu.ph>
2021-07-18 15:06:09 +0800

    Add useRef and useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+    //called once when DOM elements is rendered
+    console.log(svgRef);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg ref={svgRef}></svg>
+    
   );
 }
 
