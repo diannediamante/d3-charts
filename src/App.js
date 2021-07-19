7fafd762f318f317e6aefaf1fa08ae54a05927f4
diannediamante <mddiamante@up.edu.ph>
2021-07-19 19:39:37 +0800

    Add initial codes for GeoChart.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <Fragment>
       <h1>World Map with D3</h1>
-      {/* <GeoChart data={data} property={property} /> */}
+      <GeoChart data={data} property={property} />
       <h2>Select property to highlight</h2>
       <select value={property} onChange={event=> tSParameterProperty(event.target.value)}>
         <option value="pop_est">Population</option>
--- src/GeoChart.js
+++ src/GeoChart.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { select } from 'd3';
+import useResizeObserver from './useResizeObserver';
 
-function GeoChart() {
+function GeoChart({data, property}) {
+    const svgRef = useRef();
+    const wrapperRef = useRef();
+    const dimensions = useResizeObserver(wrapperRef);
+
+    useEffect(() => {
+        const svg = select(svgRef.current);
+
+        //use resized dimensions
+        //but fallback to getBoundingClientRect, if no dimensions yet
+        const { width, height } =
+            dimensions || wrapperRef.current.getBoundingClientRect();
+    }, [data, dimensions, property]);
+
     return (
-        <div>
+        <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+            <svg ref={svgRef}></svg>
             
         </div>
     )
