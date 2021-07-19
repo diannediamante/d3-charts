590dedb9303d42707771c475c600745e16cecd6e
diannediamante <mddiamante@up.edu.ph>
2021-07-19 19:47:54 +0800

    Add map of the world

--- src/GeoChart.js
+++ src/GeoChart.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react'
-import { select } from 'd3';
+import { select, geoPath, geoMercator } from 'd3';
 import useResizeObserver from './useResizeObserver';
 
 function GeoChart({data, property}) {
@@ -14,6 +14,20 @@ function GeoChart({data, property}) {
         //but fallback to getBoundingClientRect, if no dimensions yet
         const { width, height } =
             dimensions || wrapperRef.current.getBoundingClientRect();
+        
+        //projects geo-coordinates on a 2d plane
+        const projection = geoMercator()
+        
+        const pathGenerator = geoPath()
+            .projection(projection)
+        
+        svg
+            .selectAll(".country")
+            .data(data.features)
+            .join("path")
+            .attr("class", "country")
+            .attr("d", feature => pathGenerator(feature))
+
     }, [data, dimensions, property]);
 
     return (
